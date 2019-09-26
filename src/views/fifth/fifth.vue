abce054c2a068855fe5757dc57e89a1c0c21a159
DengLee <1007358560@qq.com>
2019-09-26 15:12:12 +0800

    092615

---
+++ src/views/fifth/fifth.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fifth",
+        data() {
+            return {}
+        },
+        methods: {},
+        created() {
+
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
